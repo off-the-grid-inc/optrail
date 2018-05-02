75bbe12a122b14ad0280b1fdf2121045aa12aee4
Álvaro Castro-Castilla <a@fourthbit.com>
2018-05-02 16:27:20 +0000

    Split manager into another file

---
+++ manager.go
@@ -0,0 +1,55 @@
+package optrail
+
+import "sync"
+
+type goId = uint64
+
+type trailManager struct {
+	sync.RWMutex
+	trails  map[goId]*opTrail
+	globals GenericMap
+}
+
+func newTrailManager() *trailManager {
+	return &trailManager{
+		trails:  make(map[goId]*opTrail),
+		globals: make(GenericMap),
+	}
+}
+
+func (m *trailManager) BeginOpTrail() *opTrail {
+	id := curGoroutineID()
+	m.Lock()
+	defer m.Unlock()
+	c := m.trails[id]
+	if c == nil {
+		c = makeOpTrail(id, nil, nil)
+		m.trails[id] = c
+	}
+	return c
+}
+
+func (m *trailManager) removeTrail(id goId) {
+	m.Lock()
+	delete(m.trails, id)
+	m.Unlock()
+}
+
+func (m *trailManager) getTrail(id goId) *opTrail {
+	m.RLock()
+	c := m.trails[id]
+	m.RUnlock()
+	return c
+}
+
+func (m *trailManager) setTrail(id goId, t *opTrail) {
+	m.Lock()
+	m.trails[id] = t
+	m.Unlock()
+}
+
+func (m *trailManager) putGlobal(key string, value interface{}) {
+	m.Lock()
+	m.globals[key] = value
+	m.Unlock()
+}
--- optrail.go
+++ optrail.go
@@ -22,58 +22,6 @@ func CurrentTrail() *opTrail {
 	return tManager.getTrail(curGoroutineID())
 }
 
-type goId = uint64
-
-type trailManager struct {
-	sync.RWMutex
-	trails  map[goId]*opTrail
-	globals GenericMap
-}
-
-func newTrailManager() *trailManager {
-	return &trailManager{
-		trails:  make(map[goId]*opTrail),
-		globals: make(GenericMap),
-	}
-}
-
-func (m *trailManager) BeginOpTrail() *opTrail {
-	id := curGoroutineID()
-	m.Lock()
-	defer m.Unlock()
-	c := m.trails[id]
-	if c == nil {
-		c = makeOpTrail(id, nil, nil)
-		m.trails[id] = c
-	}
-	return c
-}
-
-func (m *trailManager) removeTrail(id goId) {
-	m.Lock()
-	delete(m.trails, id)
-	m.Unlock()
-}
-
-func (m *trailManager) getTrail(id goId) *opTrail {
-	m.RLock()
-	c := m.trails[id]
-	m.RUnlock()
-	return c
-}
-
-func (m *trailManager) setTrail(id goId, t *opTrail) {
-	m.Lock()
-	m.trails[id] = t
-	m.Unlock()
-}
-
-func (m *trailManager) putGlobal(key string, value interface{}) {
-	m.Lock()
-	m.globals[key] = value
-	m.Unlock()
-}
-
 // OpTrail is an operation that can cross API boundaries, representation formats, storage
 // and tranmission, and remembers its history
 type OpTrail interface {
